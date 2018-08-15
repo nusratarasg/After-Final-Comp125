bf76af824215a5bf56194e52c57711ebfcc3c334
nusratarasg <nriaz2@my.centennialcollege.ca>
2018-08-15 13:55:59 -0400

    initial commit

---
+++ COMP125-S2018-FinalTest - WIP/public/Scripts/objects/contact.js
@@ -0,0 +1,16 @@
+let objects;
+(function () {
+    class Contact {
+        // your class definition goes here
+        constructor(firstName = "", lastName = "", contactNumber = "", emailAddress = "") {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.contactNumber = contactNumber;
+            this.emailAddress = emailAddress;
+        }
+    }
+
+
+    objects.Contact = Contact;
+
+})(objects || (objects = {}));
